a6fd0e8c35b7752f7043736fae231166993c0f18
Nerrit <info@nerr.it>
2022-12-23 16:38:51 +0100

    Initial Commit

---
+++ src/main/java/it/nerr/steam4j/Steam4jApplication.java
@@ -0,0 +1,13 @@
+package it.nerr.steam4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Steam4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Steam4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/nerr/steam4j/Steam4jApplicationTests.java
@@ -0,0 +1,14 @@
+package it.nerr.steam4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Steam4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert true;
+	}
+
+}
