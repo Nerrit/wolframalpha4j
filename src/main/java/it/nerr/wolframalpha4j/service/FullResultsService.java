d09517bd92c7ea3d46176035795f877242751f9c
Nerrit <info@nerr.it>
2023-01-21 00:53:00 +0100

    Refactor QueryResult

--- src/main/java/it/nerr/wolframalpha4j/service/FullResultsService.java
+++ src/main/java/it/nerr/wolframalpha4j/service/FullResultsService.java
@@ -3,7 +3,7 @@ package it.nerr.wolframalpha4j.service;
 import it.nerr.wolframalpha4j.route.Router;
 import it.nerr.wolframalpha4j.route.Routes;
 import it.nerr.wolframalpha4j.types.Format;
-import it.nerr.wolframalpha4j.types.Result;
+import it.nerr.wolframalpha4j.types.QueryResult;
 import reactor.core.publisher.Mono;
 
 public class FullResultsService extends RestService {
@@ -12,13 +12,15 @@ public class FullResultsService extends RestService {
         super(router);
     }
 
-    public Mono<Result> query(String input) {
+    private record Result(QueryResult queryResult) {}
+
+    public Mono<QueryResult> query(String input) {
         return Routes.FULL_RESULT.newRequest()
                 .query("appid", System.getenv("WOLFRAM_APP_ID"))
                 .query("input", input)
                 .query("output", Format.JSON.toString())
                 .exchange(getRouter())
-                .bodyToMono(Result.class);
+                .bodyToMono(Result.class).map(Result::queryResult);
     }
 
 }
